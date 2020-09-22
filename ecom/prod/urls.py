889c0ba4e896285665aabb53e2a3343af610b532
muhammedshafeerali <shafeeralihp@gmail.com>
2020-09-21 22:52:15 -0700

    fourth commit

--- ecom/prod/urls.py
+++ ecom/prod/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns=[
     path("",views.home,name='home'),
-    # path("login",views.login,name='login')
+    path("login",views.login,name='login'),
+    path('register',views.register,name='register'),
+    path('logout',views.logout,name='logout'),
+    path('prodv/<int:id>/',views.view,name='prodview')
     
 
 
--- ecom/prod/views.py
+++ ecom/prod/views.py
@@ -15,41 +15,47 @@ def home(request):
     glass= Product.objects.filter(product_type='sunglass')
     return render(request,'index.html',{'product': product,'tshirt':tshirt,'pant':pant,'glass':glass})
 
-# def login(request):
-#     if request.method=='POST':
-#         username=request.POST['username']
-#         password=request.POST['password']
-#         user=auth.authenticate(username=username,password=password)
-#         if user is not None:
-#              auth.login(request,user)
-#              return redirect(home)
-#         else:
-#             messages.info(request,'**invalid credentions')
-#             return HttpResponseRedirect(request.path_info)
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+             auth.login(request,user)
+             return redirect(home)
+        else:
+            messages.error(request,'**invalid credentions')
+            return HttpResponseRedirect(request.path_info)
 
-#     return render(request,'login.html')
-
-    
-# def register(request):
-#     if request.method=='POST':
-#         username=request.POST['username']
-#         email=request.POST['email']
-#         password=request.POST['password']
-#         dicti = {"username":username,"email":email}
-#         if User.objects.filter(username=username).exists():
-#             messages.info(request,'username already taken')
-#             return render(request, "login.html", dicti)
-#         elif User.objects.filter(email=email).exists():
-#             messages.info(request,'email already taken')
-#             return render(request, "login.html", dicti)
-
-#         else:
-#             user=User.objects.create_user(username=username,email=email,password=password)
-#             user.save()
-#             return redirect(login)
-#     return render(request,'login.html')
-
-
-
+    return render(request,'login.html')
 
     
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        dicti = {"username":username,"email":email}
+        if User.objects.filter(username=username).exists():
+            messages.info(request,'username already taken')
+            return render(request, "login.html", dicti)
+        elif User.objects.filter(email=email).exists():
+            messages.info(request,'email already taken')
+            return render(request, "login.html", dicti)
+
+        else:
+            user=User.objects.create_user(username=username,email=email,password=password)
+            user.save()
+            return redirect(login)
+    return render(request,'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+    
+
+def view(request,id):
+    product=Product.objects.get(id=id)
+
+    return render(request,'productview.html',{'product':product})
